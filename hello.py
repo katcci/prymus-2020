aba4891eaed57d16f3d4c0f1d138048e50b75cd9
katcci <dubielka@student.agh.edu.pl>
2021-01-06 22:53:46 +0100

    repo init

---
+++ hello.py
@@ -0,0 +1,14 @@
+print("Witaj świecie!")
+
+input_text = "testowy tekst"
+
+print(input_text)
+
+b, c = 20, 30
+
+
+new_variable = None
+if new_variable is None:
+    print("mamy pusta wartosc")
+
+ 
